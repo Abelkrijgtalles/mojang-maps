1343c65e9425be7fa8bd2572ebe2b68e73380c8e
Abel <abelvanhulst@gmail.com>
2023-07-19 14:11:09 +0200

    begin of /goto, but I want to firstly create the using file

---
+++ src/main/java/nl/abelkrijgtalles/mojangmaps/commands/GoToCommand.java
@@ -0,0 +1,31 @@
+package nl.abelkrijgtalles.mojangmaps.commands;
+
+import nl.abelkrijgtalles.mojangmaps.managers.config.NodesConfig;
+import nl.abelkrijgtalles.mojangmaps.managers.dijkstras_algorithm.Node;
+import nl.abelkrijgtalles.mojangmaps.util.NodeUtil;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class GoToCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if (commandSender instanceof Player) {
+            Player p = (Player) commandSender;
+            List<Location> locations = (List<Location>) NodesConfig.get().getList("locations");
+            locations.add(p.getLocation());
+
+            List<Node> nodes = NodeUtil.addAdjacentNodes(locations);
+
+            Node.calculateShortestPath(nodes.get(locations.size() - 1));
+        }
+
+        return true;
+    }
+}
