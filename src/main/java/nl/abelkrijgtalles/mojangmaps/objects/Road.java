58b668d21266749106d46ff4f1ea35b8283995fe
Abel <abelvanhulst@gmail.com>
2023-07-21 17:40:44 +0200

    added Road object

--- src/main/java/nl/abelkrijgtalles/mojangmaps/objects/Road.java
+++ src/main/java/nl/abelkrijgtalles/mojangmaps/objects/Road.java
@@ -1,4 +1,40 @@
 package nl.abelkrijgtalles.mojangmaps.objects;
 
+import org.bukkit.Location;
+
+import java.util.List;
+
 public class Road {
+
+    private String name;
+
+    private List<Location> locations;
+
+    public Road(List<Location> locations) {
+        this.locations = locations;
+    }
+
+    public Road(String name, List<Location> locations) {
+        this.name = name;
+        this.locations = locations;
+    }
+
+    public String getName() {
+        if (name != null) {
+            return name;
+        } else {
+            return "Unnamed Road";
+        }
+    }
+
+    public List<Location> getLocations() {
+        return locations;
+    }
+
+    public int addLocation(Location location) {
+
+        locations.add(location);
+        return locations.size() - 1;
+
+    }
 }
