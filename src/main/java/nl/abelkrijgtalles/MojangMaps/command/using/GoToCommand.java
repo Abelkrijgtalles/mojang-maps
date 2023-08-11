cae8e5cb3259e247208a110d58187304360b72c7
Abel <abelvanhulst@gmail.com>
2023-08-11 18:23:28 +0200

    changed to lambda function

--- src/main/java/nl/abelkrijgtalles/MojangMaps/command/using/GoToCommand.java
+++ src/main/java/nl/abelkrijgtalles/MojangMaps/command/using/GoToCommand.java
@@ -27,9 +27,7 @@ public class GoToCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player) {
-
-            Player p = (Player) commandSender;
+        if (commandSender instanceof Player p) {
 
             if (strings.length < 3) {
 
@@ -75,13 +73,10 @@ public class GoToCommand implements CommandExecutor {
             Node locationNode = findNodeByName(nodes, String.valueOf(NodesConfigUtil.getLocations().indexOf(closestLocationToLocation)));
 
             p.sendMessage(ChatColor.YELLOW + "Loading, this could take some time.");
-            Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
-                @Override
-                public void run() {
-                    Node.calculateShortestPath(playerNode);
-                    Node.printPaths(Collections.singletonList(locationNode), p);
-                    p.sendMessage(MessageUtil.getMessage("finallygoto").formatted(location.getBlockX(), location.getBlockZ()));
-                }
+            Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+                Node.calculateShortestPath(playerNode);
+                Node.printPaths(Collections.singletonList(locationNode), p);
+                p.sendMessage(MessageUtil.getMessage("finallygoto").formatted(location.getBlockX(), location.getBlockZ()));
             });
 
         }
