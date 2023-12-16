5d9aa622bbfb4907319dd993b9753f1deecddc55
Abel <abelvanhulst@gmail.com>
2023-12-16 16:02:29 +0100

    to degrees (cuz I haven't had radians yet :) )

--- src/main/java/nl/abelkrijgtalles/MojangMaps/util/other/BlockSelectUtil.java
+++ src/main/java/nl/abelkrijgtalles/MojangMaps/util/other/BlockSelectUtil.java
@@ -8,7 +8,7 @@ public class BlockSelectUtil {
     public static void getSelectedBlock(Location selectedLocation, Player p) {
 
         // btw, I don't even know what a rad is yet, because I haven't had/discussed it in school yet 👍
-        p.sendMessage(String.valueOf(selectedLocation.toVector().angle(p.getLocation().toVector())));
+        p.sendMessage(String.valueOf(Math.toDegrees(selectedLocation.toVector().angle(p.getLocation().toVector()))));
 
     }
 
