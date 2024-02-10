d202799a49ceda6704f4952d58e8d0f56a6752a8
Abel <abelvanhulst@gmail.com>
2024-02-10 15:58:59 +0100

    just some testing

--- src/main/java/nl/abelkrijgtalles/MojangMaps/MojangMaps.java
+++ src/main/java/nl/abelkrijgtalles/MojangMaps/MojangMaps.java
@@ -40,6 +40,7 @@ import nl.abelkrijgtalles.MojangMaps.listener.PlayerJoinListener;
 import nl.abelkrijgtalles.MojangMaps.listener.PlayerWalkListener;
 import nl.abelkrijgtalles.MojangMaps.listener.RoadCreationToolListener;
 import nl.abelkrijgtalles.MojangMaps.object.Road;
+import nl.abelkrijgtalles.MojangMaps.util.file.ConfigMigrationUtil;
 import nl.abelkrijgtalles.MojangMaps.util.file.NodesConfigUtil;
 import nl.abelkrijgtalles.MojangMaps.util.file.TranslationUtil;
 import nl.abelkrijgtalles.MojangMaps.util.other.HTTPUtil;
@@ -140,6 +141,7 @@ public final class MojangMaps extends JavaPlugin {
         ConfigurationSerialization.registerClass(Road.class);
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        getLogger().info(ConfigMigrationUtil.getConfigVersion());
         NodesConfigUtil.setup();
         TranslationUtil translationUtil = new TranslationUtil();
         translationUtil.updateTranslations();
