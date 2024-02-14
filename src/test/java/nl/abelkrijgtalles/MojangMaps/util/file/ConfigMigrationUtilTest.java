deec2e454e1d531361e916b98193735f227b3477
Abel <abelvanhulst@gmail.com>
2024-02-14 14:45:05 +0100

    added some mockbukkit inits to the first test (I have some tests some I'm not able to work so much on this)

---
+++ src/test/java/nl/abelkrijgtalles/MojangMaps/util/file/ConfigMigrationUtilTest.java
@@ -0,0 +1,43 @@
+/*
+ * MojangMaps
+ * Copyright (C) 2024 Abel van Hulst/Abelkrijgtalles/Abelpro678
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package nl.abelkrijgtalles.MojangMaps.util.file;
+
+import be.seeseemelk.mockbukkit.MockBukkit;
+import be.seeseemelk.mockbukkit.ServerMock;
+import nl.abelkrijgtalles.MojangMaps.MojangMaps;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class ConfigMigrationUtilTest {
+
+    @BeforeEach
+    public void setUp() {
+
+        ServerMock server = MockBukkit.mock();
+        MojangMaps plugin = MockBukkit.load(MojangMaps.class);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        MockBukkit.unmock();
+    }
+
+}
